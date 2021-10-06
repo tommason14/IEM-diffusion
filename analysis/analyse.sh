a75c76c9586fb82e89e6720d9254153f6569348a
Tom Mason <tom.mason14@gmail.com>
2021-10-06 17:06:58 +1100

    Calculate percentage difference and report in nice format

--- analysis/analyse.sh
+++ analysis/analyse.sh
@@ -6,6 +6,8 @@ XTC='../output.xtc'
 UXTC='unwrapped.xtc'
 TPR='em.tpr'
 
+# Remember to check Na and Cl atom names otherwise traj_convert will fail!
+
 gmx_mpi grompp -f $MDP -c $GRO -p $TOP -o $TPR
 echo System | gmx_mpi trjconv -s $TPR -f $XTC -pbc nojump -o $UXTC
 echo 2 | gmx_mpi dipoles -f $UXTC -s $TPR > dipoles.log
@@ -28,10 +30,29 @@ travis -p chlorides.pdb -i cl_input.txt && mv travis.log chloride.log
 boxsize="$(tail -1 $GRO | awk '{print ($1*$2*$3)**(1/3)*10}')"
 
 # convert to cm^2/s
-na_msd=$(grep "m^2/s" sodium.log | awk '{print $(NF-1)*10**4}')
-cl_msd=$(grep "m^2/s" chloride.log | awk '{print $(NF-1)*10**4}')
+na_diff=$(grep "m^2/s" sodium.log | awk '{print $(NF-1)*10**4}')
+cl_diff=$(grep "m^2/s" chloride.log | awk '{print $(NF-1)*10**4}')
 
 echo "# Sodium" >> corrected_diff_coeffs.txt
-corrected_diff_coeff.py -b $boxsize -d $na_msd >> corrected_diff_coeffs.txt
+corrected_diff_coeff.py -b $boxsize -d $na_diff >> corrected_diff_coeffs.txt
 echo "# Chloride" >> corrected_diff_coeffs.txt
-corrected_diff_coeff.py -b $boxsize -d $cl_msd >> corrected_diff_coeffs.txt
+corrected_diff_coeff.py -b $boxsize -d $cl_diff >> corrected_diff_coeffs.txt
+
+### Report results ###
+# sodium ran before chloride
+na_corrected=$(grep "PBCs" corrected_diff_coeffs.txt | head -1 | getcol -2)
+cl_corrected=$(grep "PBCs" corrected_diff_coeffs.txt | tail -1 | getcol -2)
+
+# CR61 values
+exp_na="2e-6"
+exp_cl="1.8e-6"
+
+perc_diff_na=$(awk -v ref="$exp_na" -v calc="$na_diff" 'BEGIN{print (ref-calc)/ref * 100}')
+perc_diff_na_corr=$(awk -v ref="$exp_na" -v calc="$na_corrected" 'BEGIN{print (ref-calc)/ref * 100}')
+perc_diff_cl=$(awk -v ref="$exp_cl" -v calc="$cl_diff" 'BEGIN{print (ref-calc)/ref * 100}')
+perc_diff_cl_corr=$(awk -v ref="$exp_cl" -v calc="$cl_corrected" 'BEGIN{print (ref-calc)/ref * 100}')
+
+cat << EOF | column -t -s,
+Dielectric,S-S dist (Å),Bjerrum length (Å),Boxsize (Å),D_Na (cm²/s),D_Cl (cm²/s),% diff Na,% diff Cl,Corrected D_Na (cm²/s),Corrected D_Cl (cm²/s),% diff Na,% diff Cl
+$dielectric,$sep,$bj,$boxsize,$na_diff,$cl_diff,$perc_diff_na,$perc_diff_cl,$na_corrected,$cl_corrected,$perc_diff_na_corr,$perc_diff_cl_corr
+EOF
